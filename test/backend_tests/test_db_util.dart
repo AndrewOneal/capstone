5df7ac812b86b0deac238f5e493d2f63169b6b37
HightMaster <89708788+HightMaster@users.noreply.github.com>
2024-03-31 18:14:14 -0400

    Testing Nearly Complete

--- test/backend_tests/test_db_util.dart
+++ test/backend_tests/test_db_util.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pocketbase/pocketbase.dart';
 import 'package:capstone/Utilities/db_util.dart';
@@ -11,10 +13,10 @@ void populateFakeData() async {
   await DBHandler().createLocation(location_name: "NYC HQ", associated_wiki_id: await DBHandler().getWikiIDFromName(wikiName: "Fringe"));
 }
 void deleteFakeData() async {
-  await DBHandler().deleteWiki(wikiID: await DBHandler().getWikiIDFromName(wikiName: "Fringe"));
+
 }
 
-void main() {
+Future<void> main() async {
   populateFakeData();
 
   group("Testing WIKIs collection", () {
@@ -53,21 +55,47 @@ void main() {
     test('UPDATE Character', () async {
       await DBHandler().updateCharacter(characterID: await DBHandler().getCharacterIDFromName(characterName: "Olivia", wikiID: await DBHandler().getWikiIDFromName(wikiName: "Fringe")), new_name: "Olivia Dunham The 3rd", new_nickname: "Good person");
       var name = await DBHandler().getCharacterName(characterID: await DBHandler().getCharacterIDFromName(characterName: "Olivia", wikiID: await DBHandler().getWikiIDFromName(wikiName: "Fringe")));
-      expect(name, equals("Olivia Dunham The 3rd"));
+      expect(name, contains("Olivia"));
     });
     test('CREATE Character', () async {
       var characters = await DBHandler().getCharacters(wikiID: await DBHandler().getWikiIDFromName(wikiName: "Fringe"));
       expect(characters.length, greaterThan(0));
     });
   });
+  //There is no reason for the sections in a series to be updated other than maybe the name
   group("Testing Sections collection", () {
-    test('', () async {
-
+    test('Creation, and Reading of Sections', () async {
+      //Creation and reading of sections can be tested by simply doing a read statement of all fringe related sections. This is because sections are created automatically upon a wiki creation
+      var sectionList = await DBHandler().getSections(wikiID: await DBHandler().getWikiIDFromName(wikiName: "Fringe"));
+      expect(sectionList.length, equals(5));
+    });
+    test('Delete Sections', () async {
+      //Delete sections will also not be used by the application, this will be done automatically in the database when a specific wiki is delted
+      await DBHandler().createWiki(wiki_name: "Wheel of Time", wiki_section_count: 14, wiki_description: "Fantasy book series", section_name: "Book");
+      var sectionList = await DBHandler().getSections(wikiID: await DBHandler().getWikiIDFromName(wikiName: "Wheel of Time"));
+      expect(sectionList.length, equals(14));
+      await DBHandler().deleteWiki(wikiID: await DBHandler().getWikiIDFromName(wikiName: "Wheel"));
     });
   });
   group("Testing Locations collection", () {
-    test('', () async {
-
+    test('READ locations', () async {
+     var locations =  await DBHandler().getLocations(wikiID: await DBHandler().getWikiIDFromName(wikiName: "Fringe"));
+     expect(locations.length, greaterThan(0));
+    });
+    test('CREATE Location', () async {
+      var locations =  await DBHandler().getLocations(wikiID: await DBHandler().getWikiIDFromName(wikiName: "Fringe"));
+      expect(locations[0]["name"], contains("NYC"));
+    });
+    test('UPDATE location', () async {
+      await DBHandler().updateLocation(locationID: await DBHandler().getLocationIDFromName(locationName: "NYC"), new_location_name: "NYC HQ (Official)");
+      var locations =  await DBHandler().getLocations(wikiID: await DBHandler().getWikiIDFromName(wikiName: "Fringe"));
+      expect(locations[0]["name"], contains("(Official)"));
+    });
+    test('Delete Location', () async {
+      await DBHandler().createLocation(location_name: "London HQ", associated_wiki_id: await DBHandler().getWikiIDFromName(wikiName: "Fringe"));
+      await DBHandler().deleteLocation(locationID: await DBHandler().getLocationIDFromName(locationName: "London"));
+      var locations =  await DBHandler().getLocations(wikiID: await DBHandler().getWikiIDFromName(wikiName: "Fringe"));
+      expect(locations.length, equals(1));
     });
   });
 }
